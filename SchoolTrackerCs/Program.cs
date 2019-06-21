87ef40972c130b393e17e1a899157b98f92787f2
Qu <Quanvle@live.dk>
2019-06-21 12:41:27 +0200

    Init School tracker project

---
+++ SchoolTrackerCs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SchoolTrackerCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
