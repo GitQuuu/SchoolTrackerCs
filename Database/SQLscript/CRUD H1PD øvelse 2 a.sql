27e3e03c6c4c9ac6c280a8b475e81cc0135b0bc5
Qu <Quanvle@live.dk>
2019-08-08 12:01:14 +0200

    more sql script

---
+++ Database/SQLscript/CRUD H1PD øvelse 2 a.sql
@@ -0,0 +1,7 @@
+﻿select * from dbo.H1PD;
+select firstName from dbo.H1PD;
+SELECT job FROM dbo.H1PD;
+SELECT firstName FROM dbo.H1PD WHERE firstName = 'Per';
+SELECT age FROM dbo.H1PD WHERE  age < 30;
+SELECT * FROM dbo.H1PD ORDER BY age ASC;
+SELECT * FROM dbo.H1PD ORDER BY age DESC;
