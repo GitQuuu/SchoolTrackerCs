6e347675fd283b9f0339153045c056e3f05eb64d
Qu <Quan0027@elec.techcollege.dk>
2019-08-15 12:03:30 +0200

    SQl script task 5

---
+++ Database/docker/SQLscript/Øvelse 5.sql
@@ -0,0 +1,13 @@
+﻿SELECT SUM(age) FROM [data-exercise5]
+WHERE firstName LIKE 'P%'
+
+SELECT SUM(age),firstName FROM [data-exercise5]
+GROUP BY firstName
+
+SELECT firstName,MAX(age) FROM [data-exercise5]
+GROUP BY  firstName
+
+SELECT firstName,MIN(age) FROM [data-exercise5]
+WHERE firstName LIKE 'P%' 
+GROUP BY firstName
+ORDER BY firstName ASC
