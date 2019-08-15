b8f63ba96926ee6f939b153eb4883d054779d5f8
Qu <Quan0027@elec.techcollege.dk>
2019-08-15 15:03:17 +0200

    SQL script Task 6
    step 1 and 2 done

---
+++ Database/docker/SQLscript/Øvelse 6.sql
@@ -0,0 +1,9 @@
+﻿SELECT firstName,Count(name) FROM Child
+RIGHT OUTER JOIN Parent on Parent.id = Child.ParentID
+WHERE firstName = 'Per'
+GROUP BY firstname
+
+SELECT firstName,Name FROM Child
+LEFT OUTER JOIN Parent on Parent.ID = Child.ParentID
+WHERE firstName = 'Per'
+
