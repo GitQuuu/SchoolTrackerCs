b35f66f3f0d409f6272700929a7629e4ac67c2fe
Qu <Quan0027@elec.techcollege.dk>
2019-08-19 13:55:42 +0200

    Remove MSQL script files
    cause too much error due to merging branches from work to home

--- Database/docker/SQLscript/Øvelse 6.sql
+++
@@ -1,9 +0,0 @@
-﻿SELECT firstName,Count(name) FROM Child
-RIGHT OUTER JOIN Parent on Parent.id = Child.ParentID
-WHERE firstName = 'Per'
-GROUP BY firstname
-
-SELECT firstName,Name FROM Child
-LEFT OUTER JOIN Parent on Parent.ID = Child.ParentID
-WHERE firstName = 'Per'
-
