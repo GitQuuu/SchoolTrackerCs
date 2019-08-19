2fb3be091d21554c2196c006fe5114154ca00cb7
Qu <Quan0027@elec.techcollege.dk>
2019-08-19 13:50:39 +0200

    SQL script task 6 100%

---
+++ Database/docker/SQLscript/Øvelse 6 100%.sql
@@ -0,0 +1,26 @@
+﻿SELECT firstName,Count(name) FROM Child
+RIGHT OUTER JOIN Parent on Parent.id = Child.ParentID
+WHERE firstName = 'Per'
+GROUP BY firstname
+
+SELECT firstName,Name FROM Child
+LEFT OUTER JOIN Parent on Parent.ID = Child.ParentID
+WHERE firstName = 'Per'
+
+SELECT firstName,SUM(Child.Age) FROM Child
+RIGHT OUTER JOIN Parent on Parent.ID = Child.ParentID
+WHERE firstName = 'Per'
+GROUP BY firstName
+
+SELECT firstName,COUNT(Child.ParentID) FROM Parent 
+INNER JOIN Child on Parent.Id = Child.ParentID
+GROUP BY firstName
+ORDER BY COUNT(Child.ParentID) ASC
+
+SELECT * FROM Parent
+FULL JOIN Child on Child.ParentID = Parent.Id
+WHERE Parent.Id IS NULL
+
+SELECT * FROM Parent
+FULL JOIN Child on Child.ParentID = Parent.Id
+WHERE Child.Id IS NULL
