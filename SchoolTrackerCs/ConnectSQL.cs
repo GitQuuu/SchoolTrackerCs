aff6fd3343164e4807fb66964559a8f2b3fa5dd3
Qu <Quan0027@elec.techcollege.dk>
2019-09-03 10:01:13 +0200

    C# and Store Procedure 100%
    Connecting to SQL database , and using a store procedure to handling the SQL queries. So that Member tables ID from SQL would be automatically put into the correct table Student or Teacher depending on the TableIDStore parameter

--- SchoolTrackerCs/ConnectSQL.cs
+++ SchoolTrackerCs/ConnectSQL.cs
@@ -52,6 +52,7 @@ namespace SchoolTrackerCs
                 {
                     command.CommandType = System.Data.CommandType.StoredProcedure;
 
+                    command.Parameters.AddWithValue("@TableIDStore", 1);
                     command.Parameters.AddWithValue("@firstName", $"{student.FirstName}");
                     command.Parameters.AddWithValue("@lastName", $"{student.LastName}");
                     command.Parameters.AddWithValue("@Grade", $"{student.Grade}");
@@ -59,7 +60,7 @@ namespace SchoolTrackerCs
                     command.Parameters.AddWithValue("@CPR", $"{student.CPR}");
                     command.Parameters.AddWithValue("@phoneNumber", $"{student.PhoneNumber}");
                     command.Parameters.AddWithValue("@email", $"{student.Email}");
-                    command.Parameters.AddWithValue("@TableIDStore", 1);
+                    
 
                     connection.Open();
                     int result = command.ExecuteNonQuery();
@@ -79,16 +80,22 @@ namespace SchoolTrackerCs
         {
             using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
             {
-                String query = "INSERT INTO dbo.Member(FirstName,LastName,Address,CPR,PhoneNumber,Email) VALUES(@FirstName,@LastName,@Address,@CPR,@PhoneNumber,@Email)";
 
-                using (SqlCommand command = new SqlCommand(query, connection))
+                using (SqlCommand command = new SqlCommand("InsertIDIntoOtherTable", connection))
                 {
+
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("@TableIDStore", 2);
                     command.Parameters.AddWithValue("@firstName", $"{teacher.FirstName}");
                     command.Parameters.AddWithValue("@lastName", $"{teacher.LastName}");
+                    command.Parameters.AddWithValue("@TeacherSalary", $"{teacher.Salary}");
+                    command.Parameters.AddWithValue("@TeacherSpecials", $"{teacher.Specials}");
                     command.Parameters.AddWithValue("@address", $"{teacher.Address}");
                     command.Parameters.AddWithValue("@CPR", $"{teacher.CPR}");
                     command.Parameters.AddWithValue("@phoneNumber", $"{teacher.PhoneNumber}");
                     command.Parameters.AddWithValue("@email", $"{teacher.Email}");
+
 
                     connection.Open();
                     int result = command.ExecuteNonQuery();
